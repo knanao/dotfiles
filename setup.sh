78f953143da9e458e29522568dfd9518e8ff0947
knanao <knanao.dev@gmail.com>
2023-05-08 23:56:39 +0900

    Update dotfile

--- setup.sh
+++ setup.sh
@@ -4,15 +4,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-DOT_FILES=(.hammerspoon bin .bash_profile .bashrc .crkbd.json .tmux.conf .vimrc .git-completion.bash starship.toml iterm2.plist)
+DOT_FILES=(.bash_profile .bashrc .crkbd.json .tmux.conf .vimrc .git-completion.bash starship.toml)
 
 for file in ${DOT_FILES[@]}
 do
+  if [[ -d $file  ]]; then
+    ln -sd $PWD/$file $HOME/$file
+  elif [[ -f $file ]]; then
     ln -sf $PWD/$file $HOME/$file
+  fi
 done
 
-brew bundle
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-curl -O https://raw.githubusercontent.com/Arc0re/Iceberg-iTerm2/master/iceberg.itermcolors
+# brew bundle
+# 
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# 
+# curl -O https://raw.githubusercontent.com/Arc0re/Iceberg-iTerm2/master/iceberg.itermcolors
